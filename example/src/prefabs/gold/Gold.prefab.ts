cbbb56c4b2a381ead9e31d36dd064eecf20b4fbd
Don <l_98b@outlook.com>
2024-02-21 13:49:35 +0800

    fix(example): modified the rules for generating gold

--- example/src/prefabs/gold/Gold.prefab.ts
+++ example/src/prefabs/gold/Gold.prefab.ts
@@ -1,5 +1,5 @@
 import { coreStore, defineContainer } from "canned-shrimp";
-import { random, sample } from "lodash-es";
+import { isEqual, random } from "lodash-es";
 import { ref } from "vue";
 import { GAME_MAP_X_LENGTH, GAME_MAP_Y_LENGTH } from "../../utils/var";
 import { GameChildrenIndex } from "../../scenes/game/game.enum";
@@ -15,34 +15,12 @@ export function defineGold() {
     const snake = coreStore.scene?.getChildAt(GameChildrenIndex.Snake) as Snake;
     const gameMap = coreStore.scene?.getChildAt(GameChildrenIndex.Map) as GameMap;
 
-    const x = random(0, GAME_MAP_X_LENGTH - 1);
-    let y: number;
-    const xInSnakeContains = snake.contains.some(([blockX]) => blockX === x);
-    if (xInSnakeContains) {
-      const contains = snake.contains
-        .filter(([blockX]) => blockX === x)
-        .sort((a, b) => a[1] - b[1]);
+    let goldPosition: BlockPosition = [random(0, GAME_MAP_X_LENGTH - 1), random(0, GAME_MAP_Y_LENGTH - 1)];
 
-      const intervals = [[0, GAME_MAP_Y_LENGTH - 1]];
+    while (snake.contains.some(contain => isEqual(goldPosition, contain))) goldPosition = [random(0, GAME_MAP_X_LENGTH - 1), random(0, GAME_MAP_Y_LENGTH - 1)];
 
-      for (const [x, y] of contains) {
-        const index = intervals.findIndex(interval => interval[1] >= y);
-
-        if (index !== -1) {
-          const [start, end] = intervals[index];
-          intervals.splice(index, 1, [start, x - 1], [x + 1, end]);
-        }
-      }
-
-      const [min, max] = sample(intervals)!;
-      y = random(min, max);
-    }
-    else {
-      y = random(0, GAME_MAP_Y_LENGTH - 1);
-    }
-
-    currentGoldPosition.value = [x, y];
-    const block = gameMap.getChildByName(createBlockName(currentGoldPosition.value)) as Block;
+    currentGoldPosition.value = goldPosition;
+    const block = gameMap.getChildByName(createBlockName(goldPosition)) as Block;
     block.updateState(BlockState.Gold);
   }
 
